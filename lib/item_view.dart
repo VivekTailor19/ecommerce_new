c2ea1df62df7deb67390866f3f999c5dc4920a89
Vivek <tailorvivekgec19@gmail.com>
2023-03-11 03:09:11 -0800

    ecommerce

--- lib/item_view.dart
+++ lib/item_view.dart
@@ -19,136 +19,147 @@ class _Item_ViewState extends State<Item_View> {
     return Scaffold(
         body: Column(
       children: [
-        Container(
-          height: 200,
-          width: 500,
-          color: Colors.black12,
-          alignment: Alignment.center,
-          child: Image.network("${info.photo}",height: 250,width: 300,fit: BoxFit.fill,),
+        Expanded(
+          child: Container(
+            height: 200,
+            width: 500,
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: Image.network("${info.photo}",height: 250,width: 300,fit: BoxFit.fill,),
+          ),
         ),
-        Container(
-          height: 250,
-          width: 500,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15))),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                        height: 15,
-                        width: 15,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.lightBlueAccent),
-                        child: Icon(Icons.local_mall,
-                            size: 12, color: Colors.white)),
-                    Container(
-                        height: 15,
-                        width: 15,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.lightBlueAccent),
-                        child: Icon(Icons.favorite,
-                            size: 12, color: Colors.white)),
-                  ],
+        Expanded(
+          child: Container(
+            height: 250,
+            width: 500,
+            decoration: BoxDecoration(
+                color: Color(0xffE2F8F7),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                          height: 30,
+                          width: 30,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.lightBlueAccent),
+                          child: Icon(Icons.local_mall,
+                              size: 22, color: Colors.white)),
+                      Container(
+                          height: 30,
+                          width: 30,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                            color: Colors.lightBlueAccent
+                          ),
+                          child: Icon(Icons.favorite,
+                              size: 22, color: Colors.white)),
+                    ],
+                  ),
                 ),
-              ),
-              Text(
-                "${info.name}",
-                style: TextStyle(
-                  fontSize: 35,
-                  color: Colors.deepPurpleAccent,
-                ),
-              ),
-              Text("${info.data}",
+                Text(
+                  "${info.name}",
                   style: TextStyle(
-                      fontSize: 15, color: Colors.deepPurpleAccent)),
-              Text(
-                "Quality",
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.deepPurpleAccent,
+                    fontSize: 30,
+                    color: Colors.deepPurpleAccent,
+                  ),
                 ),
-              ),
-              Container(
-                height: 25,
-                width: 60,
+                Text("${info.data}",
+                    style: TextStyle(
+                        fontSize: 13, color: Colors.deepPurpleAccent,),textAlign: TextAlign.center),
+                Text(
+                  "Quality",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                ),
+                Container(
+                  height: 40,
+                  width: 90,
 
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(5)),color: Colors.lightBlueAccent.shade100,),
-                child: Row(
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          count--;
-                        });
-                      },
-                      child: Container(
-                        height: 25,
-                        width: 20,
-                        child: Icon(
-                          Icons.remove,
-                          size: 15,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),color: Color(0xffA8D1DF)),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              count--;
+                            });
+                          },
+                          child: Container(
+                            height: 40,
+                            width: 25,
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.remove,
+                              size: 20,
+                            ),
+                            decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)),color: Colors.blue),
+                          ),
                         ),
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(5))),
                       ),
-                    ),
-                    Text("$count", style: TextStyle(fontSize: 15)),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          count++;
-                        });
-                      },
-                      child: Container(
-                        height: 25,
-                        width: 20,
-                        child: Icon(
-                          Icons.add,
-                          size: 15,
+                      Expanded(child: Text("$count", style: TextStyle(fontSize: 15),textAlign: TextAlign.center,)),
+                      Expanded(
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              count++;
+                            });
+                          },
+                          child: Container(
+                            height: 40,
+                            width: 25,
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.add,
+                              size: 20,
+                            ),
+                            decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)),color: Colors.blue),
+                          ),
                         ),
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(5))),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Money",
-                        style: TextStyle(
-                            fontSize: 35, color: Colors.blueAccent)),
-                    Container(
-                      height: 35,
-                      width: 120,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(10)),color: Colors.lightBlueAccent),
-                      child: Text("Add to cart",
-                          style:
-                              TextStyle(color: Colors.white, fontSize: 15)),
-                    )
-                  ],
-                ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Money",
+                          style: TextStyle(
+                              fontSize: 35, color: Colors.blueAccent)),
+                      Container(
+                        height: 35,
+                        width: 120,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(10)),color: Colors.lightBlueAccent),
+                        child: Text("Add to cart",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 15)),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         )
       ],
